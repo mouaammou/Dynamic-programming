34ceeb765b0afc32175166137a4cb686cb4baf91
mouaammou <mouadamassine@gmail.com>
2025-12-24 15:44:27 +0100

    Add memoization and tabulation implementations for bestSum function

--- DYNAMIC PROGRAMMIG/bestSum.py
+++ DYNAMIC PROGRAMMIG/bestSum.py
@@ -15,6 +15,63 @@ def bestSum(target, array):
 
    return smallest_comb
 
-print(bestSum(2, [1, 2]))
-print(bestSum(50, [2, 1, 5, 25]))
-print(bestSum(100, [2, 1, 5, 25]))
+# print(bestSum(2, [1, 2]))
+# print(bestSum(50, [2, 1, 5, 25]))
+# print(bestSum(100, [2, 1, 5, 25]))
+
+"""
+   **** BEST SOME USING MEMOIZATION ****
+"""
+
+def bestsum_memoization(target, array, memo=None):
+   if memo is None: 
+      memo = {}
+   if target in memo:
+      return memo[target]
+   if target == 0:
+      return []
+   if target < 0: 
+      return None
+   
+   smallest_combination = None
+   for num in array:
+      reminder = target - num
+      result = bestsum_memoization(reminder, array, memo)
+      if result is not None:
+         combination = result + [num]
+         if smallest_combination is None or len(combination) < len(smallest_combination):
+            smallest_combination = combination
+
+   memo[target] = smallest_combination
+   return smallest_combination
+
+print("memoization -->")
+print(bestsum_memoization(2, [1, 2]))
+print(bestsum_memoization(50, [2, 1, 5, 25]))
+print(bestsum_memoization(100, [2, 1, 5, 25]))
+print(bestsum_memoization(10000, [2, 1, 5, 25]))
+
+
+"""
+   **** best_sum using tabulation ****
+"""
+
+def best_sum_tabulation(target, array):
+   table = [None for _ in range(target + 1)]
+   table[0] = []
+   for i in range(target + 1):
+      if table[i] is not None:
+         for num in array:
+            if i + num <= target:
+               combination = table[i] + [num]
+               if table[i + num] is None or (len(table[i + num]) > len(combination)):
+                  table[i + num] = combination
+               
+   
+   return table[target]
+
+print("tabulation -->")
+print(best_sum_tabulation(2, [1, 2]))
+print(best_sum_tabulation(50, [2, 1, 5, 25]))
+print(best_sum_tabulation(100, [2, 1, 5, 25]))
+print(best_sum_tabulation(10000, [2, 1, 5, 25]))
