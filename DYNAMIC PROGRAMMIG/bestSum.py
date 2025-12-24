ac1f5c3b8d4b8f4f880dbf4fc8031d4f2e6676d5
mouaammou <mouadamassine@gmail.com>
2025-12-24 13:28:15 +0100

    Implement bestSum function to find the shortest combination of numbers that sum to the target

--- DYNAMIC PROGRAMMIG/bestSum.py
+++ DYNAMIC PROGRAMMIG/bestSum.py
@@ -1,3 +1,20 @@
 def bestSum(target, array):
-   
-   return 
+   if target == 0:
+      return []
+   if target < 0:
+      return None
+
+   smallest_comb = None
+   for num in array:
+      reminder = target - num
+      result = bestSum(reminder, array)
+      if result is not None:
+         combination = result + [num]
+         if (smallest_comb is None or len(combination) < len(smallest_comb)):
+            smallest_comb = combination
+
+   return smallest_comb
+
+print(bestSum(2, [1, 2]))
+print(bestSum(50, [2, 1, 5, 25]))
+print(bestSum(100, [2, 1, 5, 25]))
