542799d241d59df6f017e0504e0426cf4111401d
mouaammou <mouadamassine@gmail.com>
2025-12-30 21:00:57 +0100

    Refactor countResponseTimeRegressions function to eliminate average calculation and improve performance

--- SOFTWARE ENGINNER TESTS/test-01.py
+++ SOFTWARE ENGINNER TESTS/test-01.py
@@ -2,26 +2,16 @@
 # Given an array of positive integers, return the number of elements that are strictly greater than the average of all previous elements. Skip the first element.
 
 
-def average(array_of_numbers):
-    # print("array of numbers: ", array_of_numbers)
-    len_arr = len(array_of_numbers)
-    sum = 0
-    for num in array_of_numbers:
-        sum += num
-    if len_arr == 0:
-        return 0
-    return int (sum / len_arr)
-
 def countResponseTimeRegressions(responseTimes):
     count = 0
+    sum = responseTimes[0]
     for index in range(1, len(responseTimes)):
-        # print("index array: ", responseTimes[index])
-        # print("average: ", average(responseTimes[0: index]))
-        if average(responseTimes[0: index]) < responseTimes[index]:
+        avr = sum / index
+        if responseTimes[index] > avr:
             count += 1
+        sum += responseTimes[index]
     return count
-        
 
 if __name__ == '__main__':
-    result = countResponseTimeRegressions([100, 200, 150,300])
+    result = countResponseTimeRegressions(list(range(1, 100_000_000)))
     print(result)
